94f46d8b52bae818b8162e5a357f2aabf7455142
JDReal <634929844@qq.com>
2021-09-02 14:33:23 +0800

    small changes

--- src_share/vcenter_instance_check.py
+++ src_share/vcenter_instance_check.py
@@ -5,7 +5,7 @@ from pyVmomi import vim
 def vc_instance_check(cloudid):
     si = vc_login.vclogin(cloudid)
     if not isinstance(si, vim.ServiceInstance):
-        msg = "clouid 不存在，请检查。" % cloudid
+        msg = "clouid %s 不存在，请检查。" % cloudid
         error = {"ERROR": msg}
         return error
     si_content = si.RetrieveContent()
